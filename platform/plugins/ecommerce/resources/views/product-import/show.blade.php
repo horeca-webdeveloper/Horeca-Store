97494fd0b3b76c43e454ea40877544afd2a9e5df
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-01-24 13:50:57 +0530

    Update show.blade.php

--- platform/plugins/ecommerce/resources/views/product-import/show.blade.php
+++ platform/plugins/ecommerce/resources/views/product-import/show.blade.php
@@ -9,146 +9,156 @@ if ($log->description && json_validate($log->description)) {
 }
 @endphp
 
-<h6 class="section-title">{{ __("Log") }}</h6>
-<div class="db-details mb-2">
+<div class="container mt-4">
+	<div class="card">
+		<div class="card-header d-flex justify-content-between align-items-center">
+			<h3 class="mb-0">{{ __("Import Log") }}</h3>
+			<a href="{{ route('products.import') }}" class="btn btn-primary">
+				{{ __("Back to Import File") }}
+			</a>
+		</div>
+		<div class="card-body">
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("ID") }}</p>
+		<div class="db-details mb-2">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("ID") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>{{ $log->id }}</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>{{ $log->id }}</p>
-			</div>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("Module") }}</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("Module") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>{{ $log->module }}</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>{{ $log->module }}</p>
-			</div>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("Action") }}</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("Action") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>{{ $log->action }}</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>{{ $log->action }}</p>
-			</div>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("Identifier") }}</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("Identifier") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>{{ $log->identifier }}</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>{{ $log->identifier }}</p>
-			</div>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("Status") }}</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("Status") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>
+							<span class="statusStyle" @if($log->status=="Completed") style="background-color: green" @elseif($log->status=="Failed") style="background-color: red" @elseif($log->status=="In-progress") style="background-color: #d7d73f" @endif> {{ is_array($log->status) ? json_encode($log->status):$log->status }}</span>
+						</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>
-					<span class="statusStyle" @if($log->status=="Completed") style="background-color: green" @elseif($log->status=="Failed") style="background-color: red" @elseif($log->status=="In-progress") style="background-color: #d7d73f" @endif> {{ is_array($log->status) ? json_encode($log->status):$log->status }}</span>
-				</p>
-			</div>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("Created By") }}</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("Created By") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>{{ $log->createdBy->name ?? '' }}</p>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>{{ $log->createdBy->name ?? '' }}</p>
-			</div>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-md-2">
-			<div class="detailed-head">
-				<p>{{ __("Created At") }}</p>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="detailed-head">
+						<p>{{ __("Created At") }}</p>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="detailed-description">
+						<p>{{ $log->created_at }}</p>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-10">
-			<div class="detailed-description">
-				<p>{{ $log->created_at }}</p>
-			</div>
-		</div>
-	</div>
-</div>
 
-@if ($jsonDescription)
-	<h6 class="section-title">{{ __("Description") }}</h6>
-	<div class="table-responsive">
-		<table class="table">
-			<thead>
-				<tr>
-					<th width="17%">{{ __("Label") }}</th>
-					<th width="83%">{{ __("Value") }}</th>
-				</tr>
-			</thead>
-			<tbody>
-					@foreach($jsonDescription as $key => $value)
+		@if ($jsonDescription)
+			<h4 class="section-title">{{ __("Import Description") }}</h4>
+			<div class="table-responsive">
+				<table class="table">
+					<thead>
 						<tr>
-							<td><b>{{ $key }}</b></td>
-							<td>
-								@if(is_array($value) && count($value)>0)
-									@foreach($value as $key1 => $value1)
-										@if(is_array($value1))
-											@foreach($value1 as $key2 => $value2)
-												@if($key2=="Error")
-													@php($errorArray = explode(' | ', $value2))
-													@foreach($errorArray as $error)
-														&nbsp; &nbsp; &nbsp; {{$loop->iteration}}. {{ $error }}<br>
+							<th width="17%">{{ __("Label") }}</th>
+							<th width="83%">{{ __("Value") }}</th>
+						</tr>
+					</thead>
+					<tbody>
+							@foreach($jsonDescription as $key => $value)
+								<tr>
+									<td><b>{{ $key }}</b></td>
+									<td>
+										@if(is_array($value) && count($value)>0)
+											@foreach($value as $key1 => $value1)
+												@if(is_array($value1))
+													@foreach($value1 as $key2 => $value2)
+														@if($key2=="Error")
+															@php($errorArray = explode(' | ', $value2))
+															@foreach($errorArray as $error)
+																&nbsp; &nbsp; &nbsp; {{$loop->iteration}}. {{ $error }}<br>
+															@endforeach
+														@else
+															<b>{{ $key2 }}</b>: {{ is_array($value2) ? json_encode($value2):$value2 }}<br>
+														@endif
 													@endforeach
 												@else
-													<b>{{ $key2 }}</b>: {{ is_array($value2) ? json_encode($value2):$value2 }}<br>
+													<b>{{ $key1 }}</b>: {{ $value1 }}<br>
 												@endif
+												<br>
 											@endforeach
 										@else
-											<b>{{ $key1 }}</b>: {{ $value1 }}<br>
+											{{ is_array($value) ? json_encode($value):$value }}
 										@endif
-										<br>
-									@endforeach
-								@else
-									{{ is_array($value) ? json_encode($value):$value }}
-								@endif
-							</td>
-						</tr>
-					@endforeach
-			</tbody>
-		</table>
+									</td>
+								</tr>
+							@endforeach
+					</tbody>
+				</table>
+			</div>
+		@endif
 	</div>
-@endif
+</div>
 
 <style type="text/css">
 	.statusStyle {
