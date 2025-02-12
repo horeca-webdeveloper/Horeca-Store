225a625547c38e7f1d0f75a45c4cd6ebbadfa5e6
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-02-12 15:41:42 +0500

    sd

--- app/Http/Controllers/API/OrderTrackingController.php
+++ app/Http/Controllers/API/OrderTrackingController.php
@@ -127,12 +127,6 @@ class OrderTrackingController extends Controller
 			'value' => $order->shipping_method->getValue() ?? null,
 			'label' => ucfirst(str_replace('_', ' ', $order->shipping_method->getValue() ?? ''))
 		];
-		if (filter_var($product->product_image, FILTER_VALIDATE_URL)) {
-			$imageUrl = $product->product_image; // Use the full URL directly
-		} else {
-			$imageUrl = RvMedia::getImageUrl($product->product_image); // Generate full URL using RvMedia
-		}
-		
 		
 
 		/* Mapping products */
@@ -146,8 +140,10 @@ class OrderTrackingController extends Controller
 				'tax_amount' => $product->tax_amount,
 				'product_id' => $product->product_id,
 				'product_name' => $product->product_name,
-				// Add to the response
-				'product_image' => $imageUrl,
+				// 'product_image' => asset('storage/products/' . $product->product_image),
+				'product_image' => filter_var($product->product_image, FILTER_VALIDATE_URL) 
+            ? $product->product_image 
+            : RvMedia::getImageUrl($product->product_image),
 				'weight' => $product->weight,
 				'restock_quantity' => $product->restock_quantity,
 				'created_at' => $product->created_at,
