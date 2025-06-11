30aa52778d1b93294ca4feed1aa422fc1843e631
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-11 19:22:14 +0500

    sdd

--- app/Http/Controllers/API/BrandApiController.php
+++ app/Http/Controllers/API/BrandApiController.php
@@ -273,7 +273,7 @@ public function getAllHomeBrandProducts(Request $request)
                     $categoryCounts[$category->id] = [
                         'id' => $category->id,
                         'name' => $category->name,
-                        'image' => $category->image,
+                        'image' => asset('storage/' . $category->image),
                         'product_count' => 0
                     ];
                 }
