9f483c8099f812006a58c5eac081414e39b3ad73
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-11 21:48:49 +0500

    sadas

--- app/Http/Controllers/API/ProductApiController.php
+++ app/Http/Controllers/API/ProductApiController.php
@@ -444,10 +444,9 @@ class ProductApiController extends Controller
     {
 
               // Start building the base query
-                $query = Product::with(['categories', 'brand', 'tags', 'producttypes' , 'brand.products.reviews'])
+                $query = Product::with(['categories', 'brand', 'brand.products.reviews'])
                     ->where('status', 'published');
 
-                // Apply filters
                 $this->applyFilters($query, $request);
 
                 // Log query for debugging
@@ -804,8 +803,8 @@ class ProductApiController extends Controller
                         }
 
                         // Add tags and types
-                        $product->tags = $product->tags;
-                        $product->producttypes = $product->producttypes;
+                        // $product->tags = $product->tags;
+                        // $product->producttypes = $product->producttypes;
                         $product->category_list = $product->categories->map(function ($category) {
                             return [
                                 'id' => $category->id,
